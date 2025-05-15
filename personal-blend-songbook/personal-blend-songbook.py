d42754301123e011387ebee16194676f838054ec
Anthony DeCausemaker <adecause@redhat.com>
2025-05-15 09:36:42 -0400

    first commit

---
+++ personal-blend-songbook/personal-blend-songbook.py
@@ -0,0 +1,32 @@
+import os
+import json
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=PackageLoader("personal-blend-songbook"),
+    autoescape=select_autoescape()
+)
+
+def generate_index(song_list):
+    index = env.get_template("index.html")
+    index_html = index.render(song_list=song_list)
+    with open("html/index.html", 'w') as new_html:
+        new_html.write(index_html)
+    new_html.close()
+
+
+
+if __name__ == "__main__":
+    song_list = ["magic_seed", "watch_your_step"]
+    for song_name in song_list:
+        with open(os.path.join("songs", song_name + ".json"), 'r') as song_json:
+            my_song = json.load(song_json)
+        song_json.close()
+
+        song = env.get_template("song.html")
+        song_template = song.render(song=my_song)
+        with open(os.path.join("html", song_name + ".html"), 'w') as new_html:
+            new_html.write(song_template)
+        new_html.close()
+
+    generate_index(song_list=song_list)
